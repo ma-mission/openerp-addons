b4b55a920c11bfea5425e6755a325c64de4f60b1
Zakaria <zeliklil@gmail.com>
2014-02-27 12:37:25 +0000

    advancement wizard: iterate over employees instead of rules

--- hr_advancement/wizard/hr_advancement_compute.py
+++ hr_advancement/wizard/hr_advancement_compute.py
@@ -20,11 +20,18 @@
 
 from openerp.osv import fields, osv
 
-from datetime import date
+from datetime import date, datetime
 import logging
 
 
 _logger = logging.getLogger('adv_wiz')
+
+def _add_months(date_, months):
+    date_ = datetime.strptime(date_, '%Y-%m-%d')
+    month_abs = date_.year * 12 + date_.month + months - 1
+    date_ret = date(month_abs // 12, month_abs % 12 + 1, date_.day) 
+    return date_ret
+
 
 class advancementWizard(osv.osv_memory):
     _name = 'hr.advancement.wizard'
@@ -38,58 +45,51 @@ class advancementWizard(osv.osv_memory):
         'year': 2013,  # lambda a: date.today().year
     }
 
-    def _get_employee_grades(self, cr, uid, grade_id, echelon, pace, year, months):
-        _logger.info('Rule : %s, %s, %s, %d' % (grade_id, echelon, pace, months))
-        sql = "select id from hr_employee_grade where grade_id=%d and echelon=%d and date_start<%s"
-        month_ref = year * 12 + 12 - months
-        date_ref = date(month_ref // 12, month_ref % 12 + 1, 1) 
-        _logger.info(' -> Date : %s' % (date_ref,))
-        employee_grade_ids = self.pool.get('hr.employee.grade').search(
-            cr, uid, [('grade_id', '=', grade_id.id),
-                      ('echelon', '=', echelon),
-                      ('advancement_pace', '=', pace),
-                      ('date_start', '<', date_ref),
-                      ])
-        _logger.info('Employee_grades: %s' % (employee_grade_ids))
-        return employee_grade_ids or []
 
     def compute(self, cr, uid, ids, context=None):
         #year = 2014
+        unknown = 0
         year = self.browse(cr, uid, ids[0]).year
         target_year = year + 1
         rulepool = self.pool.get('hr.advancement.echelon.rule')
         proposal_pool = self.pool.get('hr.advancement.proposal')
         employee_advancement_pool = self.pool.get('hr.advancement.proposal.employee')
-        rule_ids = rulepool.search(cr, uid, [('state', '=', 'active')])
-        rules = rulepool.browse(cr, uid, rule_ids)
-        for rule in rules:
-            _logger.info('# Rule %d: %s' % (rule.id, rule.grade_id,))
-
-            slow = self._get_employee_grades(cr, uid, rule.grade_id, rule.echelon, 'S', target_year, rule.months)
-            medium = self._get_employee_grades(cr, uid, rule.grade_id, rule.echelon, 'M', target_year, rule.months_medium)
-            fast = self._get_employee_grades(cr, uid, rule.grade_id, rule.echelon, 'F', target_year, rule.months_fast)
-            employee_grade_ids = slow + medium + fast
-
-            _logger.info('Employee_grades_all: %s' % (employee_grade_ids))
-            employees = [self.pool.get('hr.employee.grade').browse(cr, uid, eg).employee_id.name for eg in employee_grade_ids]
-            _logger.info('Employees: %s' % (employees,))
-
-            if employee_grade_ids:
+        employee_grade_pool = self.pool.get('hr.employee.grade')
+        employee_grade_ids = employee_grade_pool.search(cr, uid, [('state', '=', 'current')])
+        employee_grades = employee_grade_pool.browse(cr, uid, employee_grade_ids)
+        for employee_grade in list(employee_grades):
+            rule_ids = rulepool.search(cr, uid, [('state', '=', 'active'),
+                                                ('grade_id', '=', employee_grade.grade_id.id),
+                                                ('echelon', '=', employee_grade.echelon),])
+            if not rule_ids:
+                unknown += 1
+                continue
+            rule = rulepool.browse(cr, uid, rule_ids[0])
+            pace = employee_grade.advancement_pace
+            if pace == 'F':
+                months = rule.months_fast
+            elif pace == 'M':
+                months = rule.months_medium
+            else:
+                months = rule.months
+            advancement_date = _add_months(employee_grade.date_start, months)
+            if advancement_date.year < target_year:
                 # Get or create advancement proposal
                 proposal_ids = proposal_pool.search(cr, uid, [('year', '=', year), ('grade_id', '=', rule.grade_id.id)])
                 proposal_id = (proposal_ids and proposal_ids[0] or
                                proposal_pool.create(cr, uid, {'year': year, 'grade_id': rule.grade_id.id}))
                 # add employee proposal
-                employee_grades = self.pool.get('hr.employee.grade').browse(cr, uid, employee_grade_ids)
-                for employee_grade in employee_grades:
-                    employee_advancement_proposal = {
-                        'proposal_id': proposal_id,
-                        'employee_id': employee_grade.employee_id.id,
-                        'grade_id': employee_grade.grade_id.id,
-                        'echelon': employee_grade.echelon + 1,
-                        'state': 'proposal',
-                    }
-                    employee_advancement_pool.create(cr, uid, employee_advancement_proposal)
+                employee_advancement_proposal = {
+                    'proposal_id': proposal_id,
+                    'employee_id': employee_grade.employee_id.id,
+                    'grade_id': employee_grade.grade_id.id,
+                    'echelon': employee_grade.echelon + 1,
+                    'date_start': advancement_date,  #.strftime('%Y-%m-%d'),
+                    'state': 'proposal',
+                }
+                employee_advancement_pool.create(cr, uid, employee_advancement_proposal)
+
+        _logger.info('%d rules not found!' % (unknown,))
 
         action={
             'string': 'Advancement proposals',
