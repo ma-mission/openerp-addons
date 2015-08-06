d975c9983fe3a18eeb38efe22974bf8b697fffa8
Zakaria <eliklil@uhp.ac.ma>
2015-08-06 11:42:53 +0100

    show committed amount for each budget line

--- account_budget_ma/budget.py
+++ account_budget_ma/budget.py
@@ -19,10 +19,42 @@ class budget(osv.osv):
 class budget_line(osv.osv):
     _name = 'account.budget.line'
 
+    def _committed_amount(self, cr, uid, ids, name, args, context=None):
+        res = {}
+        if context is None:
+            context = {}
+        for line in self.browse(cr, uid, ids, context=context):
+            cr.execute("SELECT SUM(amount) FROM account_analytic_line "
+                    "WHERE general_account_id=%s AND account_id=%s "
+                    "AND date BETWEEN to_date(%s,'yyyy-mm-dd') AND to_date(%s,'yyyy-mm-dd')",
+                    (line.general_account_id.id, line.analytic_axis_id.id,
+                    line.budget_id.period.date_start, line.budget_id.period.date_stop))
+            result = cr.fetchone()[0]
+            if result is None:
+                result = 0.00
+            res[line.id] = result
+        return res
+
+    def _get_budget_line_from_commitment(self, cr, uid, ids, context={}):
+        res = set()
+        for commitment in self.pool['account.analytic.line'].browse(cr, uid, ids, context=context):
+            budget_ids = self.pool['account.budget'].search(cr, uid, 
+                    [('company_id', '=', commitment.company_id.id),
+                    ('period', '=', self.pool['account.period'].find(cr, uid, commitment.date, context=context))])
+            line_ids = self.pool['account.budget.line'].search(cr, uid, 
+                    [('general_account_id', '=', commitment.general_account_id.id),
+                    ('analytic_axis_id', '=', commitment.account_id.id),
+                    ('budget_id', 'in', budget_ids)])
+            res.update(line_ids)
+        return res
+
+
     _columns = {
         'budget_id': fields.many2one('account.budget', 'Budget', required=True),
         'general_account_id': fields.many2one('account.account', 'General account', required=True),
         'analytic_axis_id': fields.many2one('account.analytic.account', 'Analytic Axis', required=True),
         'amount': fields.float('Amount', digits_compute=dp.get_precision('Account'), required=True),
+        'committed_amount': fields.function(_committed_amount, string='Committed', type='float', digits_compute=dp.get_precision('Account'),
+            store={'account.analytic.line': (_get_budget_line_from_commitment, None, 10)}),
     }
 
